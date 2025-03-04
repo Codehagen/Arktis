a1a5b883edda8007f71bc2e61186feca3be421d5
meglerhagen <Christer.hagen@gmail.com>
2025-03-04 21:19:43 +0100

    wip

--- src/app/(marketing)/blog/layout.tsx
+++ src/app/(marketing)/blog/layout.tsx
@@ -1,4 +1,4 @@
-import { Footer } from "@/components/sections/footer";
+import Footer from "@/components/sections/footer";
 import { Header } from "@/components/sections/header";
 
 interface MarketingLayoutProps {
